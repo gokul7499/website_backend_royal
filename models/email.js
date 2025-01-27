02baf6ce7e38a317f09f973e648e1ced07ac28f9
gudaghers <gudaghers@gmail.com>
2025-01-27 16:00:28 +0530

    Initial commit

--- models/email.js
+++ models/email.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const emailSchema = new mongoose.Schema({
-  email: { type: String, unique: true, required: true }, // Change field to match the route
-  subscribedAt: { type: Date, default: Date.now },
+    emailAddress: { type: String, unique: true, required: true },
+    subscribedAt: { type: Date, default: Date.now },
 });
 
 const Email = mongoose.model('Email', emailSchema);
