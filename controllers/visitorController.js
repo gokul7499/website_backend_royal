159904e00366ce3c7987c787b488ef848424b0fb
gudaghers <gudaghers@gmail.com>
2025-01-28 16:40:51 +0530

    count

--- controllers/visitorController.js
+++ controllers/visitorController.js
@@ -1,33 +1,32 @@
-const Visitor = require('../models/visitor');
+const Visitor = require("../models/visitor");
 
 // Controller to record a visitor
 const recordVisitor = async (req, res) => {
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
   try {
-    // Check if visitor already exists
+    // Check if the IP address already exists in the database
     const existingVisitor = await Visitor.findOne({ ipAddress: ip });
     if (!existingVisitor) {
+      // Create a new visitor if not found
       await Visitor.create({ ipAddress: ip });
     }
+
+    // Fetch the total number of unique visitors
     const totalVisitors = await Visitor.countDocuments();
-    res.status(200).send({
-      message: 'Visitor recorded successfully!',
-      totalVisitors,
-      visitorIP: ip,
-    });
+    res.status(200).send({ totalVisitors });
   } catch (error) {
-    res.status(500).send({ message: 'Error recording visitor', error });
+    res.status(500).send({ message: "Error recording visitor", error });
   }
 };
 
-// Controller to get total visitor count
+// Controller to get the visitor count
 const getVisitorCount = async (req, res) => {
   try {
     const totalVisitors = await Visitor.countDocuments();
     res.status(200).send({ totalVisitors });
   } catch (error) {
-    res.status(500).send({ message: 'Error fetching visitor count', error });
+    res.status(500).send({ message: "Error fetching visitor count", error });
   }
 };
 
